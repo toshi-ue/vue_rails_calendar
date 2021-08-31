5259d469be9600db97abc6330da1eccddbcfec58
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-08-31 14:47:37 +0900

    update: Vuexを使わずに2つのコンポーネントでそれぞれ取得したデータを表示する

--- frontend/src/components/CalendarDetails.vue
+++ frontend/src/components/CalendarDetails.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
-    <h1>Calendar</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">{{ event.name }}</li>
-    </ul>
-    <button type="submit" @click="fetchEvents()">fetchEvents</button>
+    <h1>CalendarDetails</h1>
+    <div v-for="event in events" :key="event.id">
+      <h2>{{ event.name }}</h2>
+      <p>start: {{ event.start }}</p>
+      <p>end: {{ event.end }}</p>
+      <p>description: {{ event.description }}</p>
+    </div>
 
-    <CalendarDetails />
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import CalendarDetails from './CalendarDetails.vue';
+
 export default {
-  name: 'Calendar',
-  components: {
-    CalendarDetails,
-  },
+  name: 'CalendarDetails',
   data: () => ({
     events: [],
   }),
