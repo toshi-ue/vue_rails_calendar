adf256209bcaee9a88f0281acb91ddac466beb98
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-09-06 11:42:27 +0900

    update: カレンダー上部に年月日を表示するように変更

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <v-sheet height="100vh">
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-toolbar-title>2021年 9月</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
       <v-calendar v-model="value" :events="events" @change="fetchEvents"></v-calendar>
     </v-sheet>
   </div>
