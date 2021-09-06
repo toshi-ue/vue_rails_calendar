c1ad0615f08ea8efb06cffe67e1741a960d4fc6a
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-09-06 10:45:06 +0900

    update: カレンダーを表示させる

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <h1>Calendar</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">{{ event.name }}</li>
-    </ul>
-    <button type="submit" @click="fetchEvents()">fetchEvents</button>
-
-    <CalendarDetails />
+    <v-sheet height="100vh">
+      <v-calendar></v-calendar>
+    </v-sheet>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import CalendarDetails from './CalendarDetails.vue';
 export default {
   name: 'Calendar',
-  components: {
-    CalendarDetails,
-  },
   computed: {
     ...mapGetters('events', ['events']),
   },
