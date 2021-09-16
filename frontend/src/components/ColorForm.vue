88e28b152e1f95ecc6f406075d0f39ed2711c958
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-09-16 11:45:48 +0900

    update(frontend): typoの修正

--- frontend/src/components/ColorForm.vue
+++ frontend/src/components/ColorForm.vue
@@ -4,7 +4,7 @@
       <template v-slot:activator="{ on }">
         <v-btn text v-on="on">
           <v-icon :color="value" size="20px">mdi-circle</v-icon>
-          <v-icon color="rgba(0, 0,0,6)" size="24px">mdi-menu-down</v-icon>
+          <v-icon color="rgba(0, 0, 0, 0.6)" size="24px">mdi-menu-down</v-icon>
         </v-btn>
       </template>
       <v-color-picker hide-canvas hide-inputs show-swatches :value="value" @input="$emit('input', $event)"> </v-color-picker>
