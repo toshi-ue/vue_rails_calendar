79722c90605e1ac5b15c9b0c6df146e3ffd6e35e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-09-28 10:21:51 +0900

    update(backend): seedデータを修正

--- backend/app/models/event.rb
+++ backend/app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  belongs_to :calendar
   validates :name, presence: true, length: {maximum: 100}
   validates :start, presence: true
   validates :end, presence: true
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,6 +1,6 @@
-Event.create!(name: '予定1', start: '2021-09-14 10:00:00', end: '2021-09-14 11:00:00')
-Event.create!(name: '予定2', start: '2021-09-14 10:00:00', end: '2021-09-14 11:00:00')
-Event.create!(name: '予定3', start: '2021-09-14 10:00:00', end: '2021-09-14 11:00:00')
-Event.create!(name: '予定4', start: '2021-09-14 10:00:00', end: '2021-09-14 11:00:00')
+calendar = Calendar.create!(name: '予定')
 
-Calendar.create!(name: '予定')
+Event.create!(name: '予定1', start: '2021-10-14 10:00:00', end: '2021-10-14 11:00:00', calendar_id: calendar.id)
+Event.create!(name: '予定2', start: '2021-10-14 10:00:00', end: '2021-10-14 11:00:00', calendar_id: calendar.id)
+Event.create!(name: '予定3', start: '2021-10-14 10:00:00', end: '2021-10-14 11:00:00', calendar_id: calendar.id)
+Event.create!(name: '予定4', start: '2021-10-14 10:00:00', end: '2021-10-14 11:00:00', calendar_id: calendar.id)
