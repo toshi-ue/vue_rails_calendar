c00ab3f08289bde75d43d22db561c2a7f49514d3
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-03 10:46:51 +0900

    update(frontend): カレンダーの作成アクションを追加

--- frontend/src/store/modules/calendars.js
+++ frontend/src/store/modules/calendars.js
@@ -4,21 +4,32 @@ import { serializeCalendar } from '../../functions/selializers';
 const apiUrl = 'http://localhost:3000';
 
 const state = {
+  calendar: null,
   calendars: [],
 };
 
 const getters = {
+  calendar: (state) => serializeCalendar(state.calendar),
   calendars: (state) => state.calendars.map((calendar) => serializeCalendar(calendar)),
 };
 
 const mutations = {
+  appendCalendar: (state, calendar) => (state.calendars = [...state.calendars, calendar]),
+  setCalendar: (state, calendar) => (state.calendars = calendar),
   setCalendars: (state, calendars) => (state.calendars = calendars),
 };
 
 const actions = {
+  async createCalendar({ commit }, calendar) {
+    const response = await axios.post(`${apiUrl}/calendars`, calendar);
+    commit('appendCalendar', response.data);
+  },
   async fetchCalendars({ commit }) {
     const response = await axios.get(`${apiUrl}/calendars`);
     commit('setCalendars', response.data);
+  },
+  async setCalendar({ commit }, calendar) {
+    commit('setCalendar', calendar);
   },
 };
 
